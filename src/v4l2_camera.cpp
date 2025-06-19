851924ecfc186b940e8a6b9d98b64f57e5c6d458
Manato HIRABAYASHI <manato.hirabayashi@tier4.jp>
2025-06-19 18:38:02 +0900

    fix: check that the frame was updated on stream liveness monitoring
    This check is needed because monitoring functions are called asynchronously
    against frame publishing.
    
    Signed-off-by: Manato HIRABAYASHI <manato.hirabayashi@tier4.jp>

--- src/v4l2_camera.cpp
+++ src/v4l2_camera.cpp
@@ -84,7 +84,7 @@ static void diagnoseStreamLiveness(
     diagnostic_updater::DiagnosticStatusWrapper &stat,
     const std::optional<uint32_t> &sequence, uint32_t &max_sequence_value,
     const std::optional<timeval> &raw_timestamp, int64_t &max_timestamp_value,
-    std::mutex& mtx)
+    bool &is_frame_updated, std::mutex& mtx)
 {
   std::lock_guard<std::mutex> lock(mtx);
   if (!sequence || !raw_timestamp) {
@@ -95,12 +95,26 @@ static void diagnoseStreamLiveness(
 
   int64_t raw_timestamp_value = static_cast<int64_t>(raw_timestamp.value().tv_sec) * 1e9
                                 + static_cast<int64_t>(raw_timestamp.value().tv_usec) * 1e3;
-  if (sequence <= max_sequence_value) {
+
+  auto is_error_condition = [&is_frame_updated](auto& lhs, auto& rhs){
+    bool compare_result;
+    if (is_frame_updated) {
+      // If frame is updated, lhs should truly greater than rhs,
+      // so error comdition will be the logical NOT.
+      compare_result = (lhs <= rhs);
+    } else {
+      // If frame is not updated yet, lhs and rhs value may the same
+      compare_result = (lhs < rhs);
+    }
+    return compare_result;
+  };
+
+  if (is_error_condition(sequence.value(), max_sequence_value)) {
     // sequence should increase monotonically
     stat.summary(diagnostic_msgs::msg::DiagnosticStatus::ERROR,
                  "Invalid sequence value is detected");
     stat.add("Stream liveness", "ERROR");
-  } else if (raw_timestamp_value <= max_timestamp_value) {
+  } else if (is_error_condition(raw_timestamp_value, max_timestamp_value)) {
     // raw timestamp should also increase monotonically
     stat.summary(diagnostic_msgs::msg::DiagnosticStatus::ERROR,
                  "Invalid timestamp value is detected");
@@ -116,6 +130,8 @@ static void diagnoseStreamLiveness(
   stat.addf("Sequence", "%u", sequence);
   stat.addf("Raw timestamp", "%lu", raw_timestamp_value);
 
+  // reset flag
+  is_frame_updated = false;
 }  // static void diagnoseStreamLiveness
 
 V4L2Camera::V4L2Camera(rclcpp::NodeOptions const & options)
@@ -242,11 +258,12 @@ V4L2Camera::V4L2Camera(rclcpp::NodeOptions const & options)
       uint32_t max_sequence_value = 0;
       std::optional<timeval> raw_timestamp{};
       int64_t max_timestamp_value = 0;
+      bool is_frame_updated = false;
       diagnostic_updater::FunctionDiagnosticTask stream_liveness_check_diag(
           "stream liveness check", std::bind(&diagnoseStreamLiveness, std::placeholders::_1,
                                              std::cref(sequence), std::ref(max_sequence_value),
                                              std::cref(raw_timestamp), std::ref(max_timestamp_value),
-                                             std::ref(lock_)));
+                                             std::ref(is_frame_updated), std::ref(lock_)));
 
       diag_composer_->addTask(&stream_liveness_check_diag);
       this->diag_updater_->add(*diag_composer_);
@@ -258,6 +275,7 @@ V4L2Camera::V4L2Camera(rclcpp::NodeOptions const & options)
         {
           std::lock_guard<std::mutex> lock(lock_);
           std::tie(img, is_v4l2_buffer_flag_error_detected, sequence, raw_timestamp) = camera_->capture();
+          is_frame_updated = true;
         }
         if (img == nullptr) {
           // Failed capturing image, assume it is temporarily and continue a bit later
